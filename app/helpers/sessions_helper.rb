220c5c7a4f80625a130f0c6d1340d147b984caa5
makidai <gachapinninaritaina@yahoo.co.jp>
2021-07-29 07:25:39 +0000

    gaman

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,8 +5,14 @@ module SessionsHelper
   end
 
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    if ( user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif(user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
 
@@ -14,7 +20,20 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  def remember(user)
+    user.remember
+    cookies.permanent[:remember_token] = user.remember_token
+    cookies.permanent.signed[:user_id] = user.id
+  end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:remember_user)
+    cookies.delete(:user_id)
+  end
+
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
