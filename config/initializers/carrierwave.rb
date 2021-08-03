02b30f10a4626438f04ef98c693524343c19bcc7
makidai <gachapinninaritaina@yahoo.co.jp>
2021-08-03 01:57:24 +0000

    s3rakusyou

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'daisuke-kenkou2' # 作成したバケット名を記述
+    config.fog_directory  = 'daisuke-kenkou3' # 作成したバケット名を記述
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY'], # 環境変数
